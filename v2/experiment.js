adbee03cd7e8053583bf2b1899a847da0dd483bc
Volkan Durmaz <vdurmaz@gmail.com>
2022-12-21 12:37:42 +0300

    Update

--- v2/experiment.js
+++ v2/experiment.js
@@ -2,92 +2,47 @@ var song
 var fft
 var particles = []
 var particles2 = []
-var particles3 = []
 
 function preload() {
     song = loadSound("./object.mp3")  
 }
 
 function setup() {
-    createCanvas(window.innerWidth, window.innerHeight);
+    createCanvas(windowWidth, windowHeight);
     angleMode(DEGREES)
     rectMode(CENTER)
     fft = new p5.FFT(0.8, 512)
     noLoop()
-    colorx = random(0,100)
-    colory = random(0,100)
-    colorz = random(255)
-    colorx1 = random(255)
-    colory1 = random(255)
-    colorz1 = random(255)
-    background(colorx, colory, colorz);
+    background(random(255), random(255), random(255));
+    
 }
-
-
 
 function draw() {
     translate(width/2, height/2)
     fft.analyze()
     amp = fft.getEnergy(1, 1000)
 
-    var alpha = map(amp, 0, 255, 0, 150)
-    fill(colorx, colory, colorz, alpha)
+    var alpha = map(amp, 0, 255, 100, 150)
+    fill(0, alpha)
     noStroke()
     rect(0, 0, width, height)
 
     stroke(random(255), random(255), random(255))
-    strokeWeight(random(0,30))
+    strokeWeight(random(1,50))
     noFill()
 
     var wave = fft.waveform()
 
     for(var t = -1; t <= 1; t += 2) {
         beginShape()
-        for(var i = 0; i <= 180; i += 1) {
+        for(var i = 0; i <= 180; i += 0.5) {
             var index = floor(map(i,0,180,0,wave.length-1))
-            var r = map(wave[index], -1, 1, 90, window.innerWidth/4)
+            var r = map(wave[index], -1, 1, 90, 350)
             var x = r * sin(i) * t
             var y = r * cos(i)
             vertex(x,y)
         }
         endShape()
-    }
-    for(var t = -1; t <= 1; t += 2) {
-        beginShape()
-        for(var i = 0; i <= 180; i += 2) {
-            var index = floor(map(10,-10,180,0,wave.length-1))
-            var r = map(wave[index], -1, 1, 5, window.innerWidth/30)
-            var x = r * sin(i) * t
-            var y = r * cos(i)
-            vertex(x,y)
-        }
-        endShape()
-    }
-
-    for(var t = -1; t <= 1; t += 2) {
-        beginShape()
-        for(var i = 0; i <= 180; i += 1) {
-            var index = floor(map(i,0,180,0,wave.length-1))
-            var r = map(wave[index], -1, 1, 90, window.innerWidth/2)
-            var x = r * sin(i) * t
-            var y = r * cos(i)
-            vertex(x,y)
-        }
-        endShape()
-    }
-    
-
-    var p = new Particle3()
-    particles3.push(p)
-
-    for(var i = particles3.length - 1; i >= 0; i--) {
-        if(!particles3[i].edges()) {
-            particles3[i].update(amp > 1000)
-            particles3[i].show()
-        } else {
-            particles3.splice(1, i)
-        }
-        
     }
 
     var p = new Particle()
@@ -128,46 +83,14 @@ function mouseClicked() {
     }
 }
 
-class Particle3{
-    constructor() {
-        this.pos = p5.Vector.random2D().mult(100)
-        this.vel = createVector(random(0),random(0))
-        this.acc = this.pos.copy().mult(random(0.001, 0.00001))
-
-        this.w = random(1, 15)
-        this.color = [colorx1, colory1, colorz1]
-    }
-    update(cond) {
-        this.vel.add(this.acc)
-        this.pos.add(this.vel)
-        if(cond) {
-            this.pos.add(this.vel)
-            this.pos.add(this.vel)
-            this.pos.add(this.vel)
-        }
-    }
-    edges() {
-        if(this.pos.x < -width/2 || this.pos.x > width/2 || this.pos.y < -height/2 || this.pos.y > height/2) {
-            return true
-        } else {
-            return false
-        }
-    }
-    show() {
-        noStroke()
-        fill(this.color)
-        square(this.pos.x, this.pos.y, this.w)
-    }
-}
-
 class Particle{
     constructor() {
-        this.pos = p5.Vector.random2D().mult(50)
+        this.pos = p5.Vector.random2D().mult(20)
         this.vel = createVector(random(0),random(0))
         this.acc = this.pos.copy().mult(random(0.0001, 0.00001))
 
         this.w = random(1, 15)
-        this.color = [random(0,255), random(0,255), random(150,150)]
+        this.color = [random(0,255), random(50,150), random(150,150)]
     }
     update(cond) {
         this.vel.add(this.acc)
@@ -194,12 +117,12 @@ class Particle{
 
 class Particle2{
     constructor() {
-        this.pos = p5.Vector.random2D().mult(300)
+        this.pos = p5.Vector.random2D().mult(400)
         this.vel = createVector(0,0)
-        this.acc = this.pos.copy().mult(random(0.01, 0.00001))
+        this.acc = this.pos.copy().mult(random(0.1, 0.00001))
 
         this.w = random(1, 10)
-        this.color = [random(0,255), random(0,255), random(0,255)]
+        this.color = [random(200,255), random(200,255), random(200,255)]
     }
     update(cond) {
         this.vel.add(this.acc)
@@ -211,7 +134,7 @@ class Particle2{
         }
     }
     edges() {
-        if(this.pos.x < -width/2 || this.pos.x > width || this.pos.y < -height/2 || this.pos.y > height/2) {
+        if(this.pos.x < -width/2 || this.pos.x > width/2 || this.pos.y < -height/2 || this.pos.y > height/2) {
             return true
         } else {
             return false
